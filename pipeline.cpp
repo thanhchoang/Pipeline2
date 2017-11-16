ceb3ef76a2447f0d14990a806d4e7b65ddf9729f
thanhchoang <30697151+thanhchoang@users.noreply.github.com>
2017-11-16 09:50:57 -0600

    Add files via upload

---
+++ pipeline.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	double firstnum = 10;
+	double secondnum = 20;
+	double thirdnum = 100;
+
+	cout << firstnum << " ---- " << secondnum << " ----" << thirdnum << " -----" << endl;
+
+	double result = firstnum * secondnum * thirdnum;
+	cout << "The result is: " << result << endl;
+
+	return 0;
+}
